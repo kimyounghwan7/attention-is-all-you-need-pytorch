60889a7b0fa9ad885766b868e982e32b17fd00ef
kimyounghwan7 <peww99@gmail.com>
2025-07-09 22:36:18 +0900

    202507092232_bill_tab_fix

--- transformer/Modules.py
+++ transformer/Modules.py
@@ -5,21 +5,21 @@ import torch.nn.functional as F
 __author__ = "Yu-Hsiang Huang"
 
 class ScaledDotProductAttention(nn.Module):
-    ''' Scaled Dot-Product Attention '''
+	''' Scaled Dot-Product Attention '''
 
-    def __init__(self, temperature, attn_dropout=0.1):
-        super().__init__()
-        self.temperature = temperature
-        self.dropout = nn.Dropout(attn_dropout)
+	def __init__(self, temperature, attn_dropout=0.1):
+		super().__init__()
+		self.temperature = temperature
+		self.dropout = nn.Dropout(attn_dropout)
 
-    def forward(self, q, k, v, mask=None):
+	def forward(self, q, k, v, mask=None):
 
-        attn = torch.matmul(q / self.temperature, k.transpose(2, 3))
+		attn = torch.matmul(q / self.temperature, k.transpose(2, 3))
 
-        if mask is not None:
-            attn = attn.masked_fill(mask == 0, -1e9)
+		if mask is not None:
+			attn = attn.masked_fill(mask == 0, -1e9)
 
-        attn = self.dropout(F.softmax(attn, dim=-1))
-        output = torch.matmul(attn, v)
+		attn = self.dropout(F.softmax(attn, dim=-1))
+		output = torch.matmul(attn, v)
 
-        return output, attn
+		return output, attn
--- transformer/__init__.py
+++ transformer/__init__.py
@@ -7,6 +7,6 @@ import transformer.Translator
 import transformer.Optim
 
 __all__ = [
-    transformer.Constants, transformer.Modules, transformer.Layers,
-    transformer.SubLayers, transformer.Models, transformer.Optim,
-    transformer.Translator]
+	transformer.Constants, transformer.Modules, transformer.Layers,
+	transformer.SubLayers, transformer.Models, transformer.Optim,
+	transformer.Translator]
